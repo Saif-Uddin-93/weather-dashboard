9c3e2f09f1a932b06cba3817a11537ed3994ca16
Saif Uddin <admin@saifuddin.uk>
2024-01-14 21:11:30 +0000

    changed API call to metric units

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
 const city = "london";
-const countryCode = "UK";
+const country = "UK";
 const apiKey = "f7755e3d7158d958bc9cd2b4fee96a47";
-const openWeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&APPID=${apiKey}`;
+const openWeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${apiKey}&units=metric`;
 
 const exampleAPIResult = 
-{"coord":
-    {"lon":-0.1257,"lat":51.5085},
+{
+    "coord":{
+        "lon":-0.1257,
+        "lat":51.5085},
     "weather":[{
-        "id":802,
+        "id":803,
         "main":"Clouds",
-        "description":"scattered clouds",
-        "icon":"03n"
-    }],
+        "description":"broken clouds",
+        "icon":"04n"}],
     "base":"stations",
     "main":{
-        "temp":277.57,
-        "feels_like":273.69,
-        "temp_min":276.24,
-        "temp_max":278.42,
+        "temp":4.07,
+        "feels_like":1.32,
+        "temp_min":2.9,
+        "temp_max":4.98,
         "pressure":1009,
-        "humidity":83
-    },
+        "humidity":83},
     "visibility":10000,
-    "wind":
-    {
-        "speed":5.14,"deg":290
-    },
-    "clouds":
-    {
-        "all":40
-    },
-    "dt":1705264073,
-    "sys":
-    {
+    "wind":{
+        "speed":3.09,
+        "deg":360},
+    "clouds":{
+        "all":75},
+    "dt":1705266216,
+    "sys":{
         "type":2,
         "id":2075535,
         "country":"GB",
         "sunrise":1705219240,
-        "sunset":1705249051
-    },
+        "sunset":1705249051},
     "timezone":0,
     "id":2643743,
     "name":"London",
-    "cod":200,
+    "cod":200
 }
-            
+
+$(".city-name").text(exampleAPIResult.name);
+$("#today .date").text(dayjs().format("DD/MM/YYYY"));
+$("#today .temp").text(exampleAPIResult.main.temp);
+$("#today .wind").text(exampleAPIResult.wind.speed);
+$("#today .humidity").text(exampleAPIResult.main.humidity);
