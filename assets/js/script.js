348d83a9aa1abf9ca02ff63b3976aa00e7293045
Saif Uddin <admin@saifuddin.uk>
2024-01-30 12:19:57 +0000

    removed while loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,7 @@ function day(index=0){
 }
 
 function noonIndex(result, index, max){
-    while (index>max-1)index--;
+    if (index>max-1)index=max-1;
     const oneDay = 86400;
     const timestamp = result.list[index].dt;
     // look for index at 12pm. half of oneDay is 12PM
